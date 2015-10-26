59e7157c31358b0f64074afd461895013fb32512
johnhur <johnjhur@gmail.com>
2015-10-26 16:31:37 -0700

    form validations and post request working

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,17 +1,39 @@
 app.controller("loginController", ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http){
-	$scope.test = "Login Controller"
+
+	$('.ui.checkbox')
+  .checkbox() // consent checkbox on form
+
+  $scope.consent = false 
+
+  $scope.toggleConsent = function(){ // used for ng-hide conditional to display button 
+  	$scope.consent = !$scope.consent
+  }
+
+  $scope.postConsent = function(){ // post request for volunteer
+  	var volunteer = {}
+
+  	volunteer.firstname = $scope.firstname
+  	volunteer.lastname = $scope.lastname
+  	volunteer.consent = $scope.consent
+  	volunteer.city = $scope.city
+
+  	$http.post('/js/provider.json', volunteer).success(function(response){
+  		console.log(response + " for volunteer consent")
+  		console.log(volunteer)
+  	})
+  }
+
+;
 
 }])
 
 app.controller("searchController", ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http){
 
-	$scope.test = "Search Controller"
-
-	$http.get('/js/provider.json').then(function(response){
+	$http.get('/js/provider.json').then(function(response){ // gets physician/organization data
 		$scope.data = response.data;
 	})
 
-	$scope.postInfo = function(){
+	$scope.postInfo = function(){ // post request with physician/organization selected
 		var info = this.object
 		$http.post('/js/provider.json', info).success(function(response){
 			console.log(response)
